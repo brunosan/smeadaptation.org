f6d8d2f2be25613b4082dbf24154785ffb9e171a
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2013-04-11 13:50:06 -0400

    prepring basic carousel

---
+++ _site/lib/js/js.js
@@ -0,0 +1,15 @@
+//carousel
+!function ($) {
+  $(function(){
+  // carousel demo
+  $('#myCarousel').carousel({  
+    interval: 2000 // in milliseconds  
+  })
+  })
+}(window.jQuery)
+
+//$("#carousel_nav2").click(function(){   
+//  var item = 2;   
+//  $('#home_carousel').carousel(item);   
+//  return false;   
+//});
